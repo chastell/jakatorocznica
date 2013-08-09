ce5fecc991c80fb5dd1dc48b92ef4afbc8506b22
Piotr Szotkowski <chastell@chastell.net>
2013-08-09 09:19:11 +0200

    update after latest workshop

--- jakatorocznica.rb
+++ jakatorocznica.rb
@@ -1,13 +1,24 @@
 # encoding: UTF-8
 
+Anniversary = Struct.new(:date, :name)
+
 require 'sinatra'
 
 get '/' do
-	if Date.today.day == 1 and Date.today.month == 8
-		'Dziś jest rocznica Powstania Warszawskiego.'
-	elsif Date.today.day == 11 and Date.today.month == 11
-		'Dziś jest Dzień Niepodległości.'
-	else
-		'Nie ma dziś żadnej rocznicy.'
-	end
+	@lenin = Anniversary.new(
+		Date.new(1914, 8, 8),
+		'aresztowanie Lenina w Nowym Targu'
+	)
+	@wielopolski = Anniversary.new(
+		Date.new(1862, 8, 7),
+		'zamach na Wielopolskiego'
+	)
+	@edison = Anniversary.new(
+		Date.new(1892, 8, 9),
+		'patent Edisona na telegraf'
+	)
+	@anniversaries = [@wielopolski, @lenin, @edison]
+	@current_anniversary = @wielopolski
+	@anniversary_number = Date.today.year - @current_anniversary.date.year
+	erb :index
 end
