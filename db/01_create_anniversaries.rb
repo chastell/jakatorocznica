6f89aaf349c6baca95d0902785dd9b42f8b6ad0d
Tomasz Stachewicz <tomekrs@o2.pl>
2013-09-05 18:17:59 +0200

    date is a good field name

--- db/01_create_anniversaries.rb
+++ db/01_create_anniversaries.rb
@@ -1,7 +1,7 @@
 class CreateAnniversaries < ActiveRecord::Migration
   def change
     create_table :anniversaries do |table|
-      table.date :original_date
+      table.date :date
       table.string :name
     end
   end
