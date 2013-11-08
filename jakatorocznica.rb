b930e8b6117d11cbd183bb8999745d19cd218eee
Piotr Szotkowski <chastell@chastell.net>
2013-11-08 19:52:43 +0100

    update after November 8th workshop

--- jakatorocznica.rb
+++ jakatorocznica.rb
@@ -33,17 +33,40 @@ get '/' do
     Date.new(1660, 11, 2),
     'koniec bitwy pod Cudnowem'
   )
-  @anniversaries = [wielopolski, lenin, edison, szczepanski, linia_kolejowa, andrychow, bitwa]
+  katedra = Anniversary.new(
+    Date.new(1144, 11, 8),
+    'konsekracja katedry płockiej'
+  )
+  telefony = Anniversary.new(
+    Date.new(1889, 11, 9),
+    'pierwsze telefony w Gdańsku'
+  )
+  spis = Anniversary.new(
+    Date.new(1950, 12, 3),
+    'spis powszechny'
+  )
+  @anniversaries = [wielopolski, lenin, edison, szczepanski, linia_kolejowa,
+   andrychow, bitwa, katedra, telefony, spis].shuffle
 
-  @anniversaries.each do |anniversary|
-    if anniversary.date.day == Date.today.day and anniversary.date.month == Date.today.month
-      @current_anniversary = anniversary
-      @anniversary_number = Date.today.year - @current_anniversary.date.year
-    end
-    if anniversary.date.month < Date.today.month or (anniversary.date.month == Date.today.month and anniversary.date.day < Date.today.day)
-      @last_anniversary = anniversary
-      @last_anniversary_number = Date.today.year - @last_anniversary.date.year
-    end
+  @anniversaries = @anniversaries.sort_by do |anniversary|
+    [anniversary.date.month, anniversary.date.day]
+  end
+
+  @current_anniversary = @anniversaries.find do |anniversary|
+    anniversary.date.day == Date.today.day and
+      anniversary.date.month == Date.today.month
+  end
+
+  @next_anniversary = @anniversaries.find do |anniversary|
+    anniversary.date.month > Date.today.month or
+      (anniversary.date.month == Date.today.month and
+       anniversary.date.day > Date.today.day)
+  end
+
+  @last_anniversary = @anniversaries.reverse.find do |anniversary|
+    anniversary.date.month < Date.today.month or
+      (anniversary.date.month == Date.today.month and
+       anniversary.date.day < Date.today.day)
   end
 
   erb :index
