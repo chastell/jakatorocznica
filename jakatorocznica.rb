f158bae59a14b1faab66a17bf44fc55fb5476649
Tomasz Stachewicz <tomekrs@o2.pl>
2013-11-15 19:11:28 +0100

    CONSTANTS. I HAZ DEM.

--- jakatorocznica.rb
+++ jakatorocznica.rb
@@ -4,49 +4,52 @@ Anniversary = Struct.new(:date, :name)
 
 require 'sinatra'
 
-get '/' do
-  lenin = Anniversary.new(
+ANNIVERSARIES = [
+  Anniversary.new(
     Date.new(1914, 8, 8),
     'aresztowanie Lenina w Nowym Targu'
-  )
-  wielopolski = Anniversary.new(
+  ),
+  Anniversary.new(
     Date.new(1862, 8, 7),
     'zamach na Wielopolskiego'
-  )
-  szczepanski = Anniversary.new(
+  ),
+  Anniversary.new(
     Date.new(1944, 8, 29),
     'Józef Szczepański napisał wiersz Czerwona zaraza'
-  )
-  edison = Anniversary.new(
+  ),
+  Anniversary.new(
     Date.new(1892, 8, 9),
     'patent Edisona na telegraf'
-  )
-  linia_kolejowa = Anniversary.new(
+  ),
+  Anniversary.new(
     Date.new(1861, 10, 24),
     'otwarcie linii kolejowej Toruń-Bydgoszcz'
-  )
-  andrychow = Anniversary.new(
+  ),
+  Anniversary.new(
     Date.new(1767, 10, 24),
     'nadanie Andrychowowi praw miejskich'
-  )
-  bitwa = Anniversary.new(
+  ),
+  Anniversary.new(
     Date.new(1660, 11, 2),
     'koniec bitwy pod Cudnowem'
-  )
-  katedra = Anniversary.new(
+  ),
+  Anniversary.new(
     Date.new(1144, 11, 8),
     'konsekracja katedry płockiej'
-  )
-  telefony = Anniversary.new(
+  ),
+  Anniversary.new(
     Date.new(1889, 11, 9),
     'pierwsze telefony w Gdańsku'
-  )
-  spis = Anniversary.new(
+  ),
+  Anniversary.new(
     Date.new(1950, 12, 3),
     'spis powszechny'
   )
-  @anniversaries = [wielopolski, lenin, edison, szczepanski, linia_kolejowa,
-   andrychow, bitwa, katedra, telefony, spis].shuffle
+]
+
+get '/' do
+  
+  @anniversaries = ANNIVERSARIES.shuffle
 
   @anniversaries = @anniversaries.sort_by do |anniversary|
     [anniversary.date.month, anniversary.date.day]
